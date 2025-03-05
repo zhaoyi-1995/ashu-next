a96d285c4fa9a846540e9113b1fd456bbc5435ba
zhaoyi-1995 <492613075@qq.com>
2025-03-05 23:22:31 +0800

    分饱

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: true,
+  webpack(config) {
+    config.optimization.splitChunks = {
+      chunks: 'all', // 对所有 chunk 进行分割
+      maxSize: 1000000, // 每个 chunk 最大 1MB，强制分割大文件
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
